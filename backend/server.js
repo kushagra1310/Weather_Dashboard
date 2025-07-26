2d60e69d61b2c7acfb66851bcc8f0495aafd27da
kushagra1310 <kushagraagrawalashoka@gmail.com>
2025-07-26 11:41:56 +0530

    Frontend and backend added

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors")
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+const API_key = process.env.API_KEY;
+app.get("/weather", async (req, res) => {
+    const city_name = req.query.city;
+    if (city_name) {
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+                    city_name
+                )}&appid=${API_key}&units=metric`
+            );
+            const data = await response.json();
+            console.log(data);
+            if (!response.ok) {
+                return res.status(response.status).json({ error: data.message || "Weather API error" });
+            }
+
+            res.json({data});
+        }
+        catch (error) {
+            res.status(500).json({ error: "Failed to fetch weather data" });
+        }
+    }
+    else
+        return res.status(400).json({ error: "City is required" });
+});
+app.listen(3000, () => {
+    console.log("Server is running  at http://localhost:3000")
+});
---
+++ public/animation.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("submission").addEventListener("click", async () => {
+        const city_name = document.getElementById("cityinput").value.trim();
+        const tempDisplay = document.getElementById("temp_display");
+        if (!city_name) {
+            tempDisplay.innerHTML = "⚠️ Please enter a city name.";
+            return;
+        }
+
+        tempDisplay.innerHTML = "⏳ Loading weather data...";
+
+        try {
+            const response=await fetch(`http://localhost:3000/weather?city=${city_name}`);
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+            tempDisplay.innerHTML = `
+        🌡️ <strong>Temperature (Max):</strong> ${data.data.main.temp_max}°C<br>
+        🧊 <strong>Temperature (Min):</strong> ${data.data.main.temp_min}°C<br>
+        💧 <strong>Humidity:</strong> ${data.data.main.humidity}%<br>
+        🌬️ <strong>Wind Speed:</strong> ${data.data.wind.speed} m/s
+      `;
+        } catch (error) {
+            tempDisplay.innerHTML = `❌ Error: ${error.message}`;
+            console.error(error);
+        }
+    });
+});
